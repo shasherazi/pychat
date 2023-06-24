bc640419a17ed949ab10f1780ebbfa8c24ec2bfc
Arslan909 <arslanswl909@gmail.com>
2023-06-25 02:21:41 +0500

    added support for sending multiple text from both sides at the same time

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = "127.0.0.1"
@@ -6,13 +7,28 @@ port = 12345
 
 s.connect((ip_address, port))
 
+globalMsg = ""
+class Recive_Thread(Thread):
+    def run(self):
+        while True:
+            msg = s.recv(1024)
+            print(msg.decode())
+            
+            globalMsg = msg.decode()
 
-while True:
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
-    if msg == "exit":
-        break
-    msg = input("Enter message: ")
-    s.send(msg.encode("utf-8"))
+        
+class Send_Thread(Thread):
+    def run(self):
+        while True:
+            msg = input("Enter message: ")
+            s.send(msg.encode())
+                
+t1 = Recive_Thread()
+t2 = Send_Thread()
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 s.close()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = "127.0.0.1"
@@ -8,13 +9,28 @@ s.listen(5)
 c, addr = s.accept()
 print("Connection from: " + str(addr))
 
+globalMsg = ""
+class Recive_Thread(Thread):
+    def run(self):
+        while True:
+            msg = c.recv(1024)
+            print(msg.decode())
+            
+            
+class Send_Thread(Thread):
+    def run(self):
+        while True:
+                msg = input("Enter message: ")
+                c.send(msg.encode())
+            
 
-while True:
-    msg = input("Enter message: ")
-    c.send(msg.encode("utf-8"))
-    if msg == "exit":
-        break
-    msg = c.recv(1024)
-    print(msg.decode("utf-8"))
+t1 = Recive_Thread()
+t2 = Send_Thread()
+t1.start()
+t2.start()
+
+
+t1.join()
+t2.join()
 
 c.close()
